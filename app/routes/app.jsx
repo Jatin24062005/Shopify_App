8096c008477d6d527611c4621dee0e6f477ec0ab
JATIN <Jatin04072005@gmail.com>
2026-02-16 01:31:32 +0530

    improving error model

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -3,7 +3,6 @@ import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
 
-
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
@@ -26,9 +25,34 @@ export default function App() {
 }
 
 // Shopify needs React Router to catch some thrown responses, so that their headers are included in the response.
-export function ErrorBoundary() {
-  return boundary.error(useRouteError());
+
+
+export function ErrorBoundary({
+  error,
+}) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <>
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
+
 
 export const headers = (headersArgs) => {
   return boundary.headers(headersArgs);
